a6188c4c3809adf5effc6fa73988f99ce4598e19
voltion01 <a.anton.2021@alumnos.urjc.es>
2024-04-25 17:10:40 +0200

    login

---
+++ Templates/login/eyehide.js
@@ -0,0 +1,54 @@
+function togglePasswordVisibility() {
+    var passwordInput = document.getElementById("passwordInput");
+    var toggleButton = document.getElementById("eyeIcon");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        toggleButton.src = "/login/img/view.png";
+    } else {
+        passwordInput.type = "password";
+        toggleButton.src = "/login/img/hide.png";
+    }
+}
+function togglePasswordVisibilityTwo() {
+    var passwordInput = document.getElementById("passwordInputTwo");
+    var toggleButton = document.getElementById("eyeIconTwo");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        toggleButton.src = "/login/img/view.png";
+    } else {
+        passwordInput.type = "password";
+        toggleButton.src = "/login/img/hide.png";
+    }
+}
+
+function PassRequirements (){
+    var passwordInput = document.getElementById("passwordInput").value;
+    var caracterEspecial = /[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/;
+    var submitButton = document.querySelector("#SignUp button[type='submit']");
+
+
+    if(passwordInput.length >= 8){
+        document.getElementById("requirements0").style.color="green";
+    }
+    else{
+
+        document.getElementById("requirements0").style.color="red";
+    }
+
+    if(caracterEspecial.test(passwordInput)){
+        document.getElementById("requirements1").style.color="green";
+    }
+    else{
+        document.getElementById("requirements1").style.color="red";
+
+    }
+    if(caracterEspecial.test(passwordInput) && passwordInput.length >= 8){
+        submitButton.disabled = false;
+    }
+    else{
+        submitButton.disabled = true;
+    }
+
+}
