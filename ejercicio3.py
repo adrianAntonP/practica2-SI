16783b3d1f837d7ec3b203004a80b044703b17fd
erikarusa <161024556+erikarusa@users.noreply.github.com>
2024-04-26 12:46:34 +0200

    update 3

--- ejercicio3.py
+++ ejercicio3.py
@@ -3,19 +3,19 @@ import requests
 def last_10_vulnerabilities():
     url = "https://cve.circl.lu/api/last"
     try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            return response.json()[:10]
+        aux = requests.get(url)
+        if aux.status_code == 200:
+            return aux.json()[:10]
         else:
-            print("Error al obtener los datos de la API:", response.status_code)
+            print("error:", aux.status_code)
             return None
     except requests.exceptions.RequestException as e:
-        print("Error de conexión:", e)
+        print("error:", e)
         return None
 
 last_vulnerabilities = last_10_vulnerabilities()
 if last_vulnerabilities:
     for i, vuln in enumerate(last_vulnerabilities, 1):
-        print(f"{i}. CVE: {vuln['id']} - Descripción: {vuln.get('summary', 'No disponible')}")
+        print(f"{i}. CVE: {vuln['id']} - Descripción: {vuln.get('summary')}")
 else:
-    print("No se pudieron obtener las últimas vulnerabilidades.")
+    print("error")
